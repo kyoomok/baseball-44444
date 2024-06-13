e472c53c459b8e475e0424ae124c65c957fde7c4
km4.kim <kyoomok@naver.com>
2024-06-13 14:08:19 +0900

    [refactoring] 41

--- game.py
+++ game.py
@@ -4,4 +4,11 @@ class Game:
             raise TypeError()
 
         if len(guess_num) != 3:
-            raise TypeError
+            raise TypeError()
+
+        for num in guess_num:
+            if not ord('0') <= ord(num) < ord('9'):
+                raise TypeError()
+
+        if guess_num[0] == guess_num[1] or guess_num[0] == guess_num[2] or guess_num[1] == guess_num[2]:
+            raise TypeError()
--- test_game.py
+++ test_game.py
@@ -19,3 +19,6 @@ class TestGame(TestCase):
         self.assert_illegal_argument(None)
         self.assert_illegal_argument("12")
         self.assert_illegal_argument("1234")
+        self.assert_illegal_argument("12s")
+        self.assert_illegal_argument("121")
+
