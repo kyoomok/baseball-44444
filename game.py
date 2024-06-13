57e6973f357390ce5823093f81f93dd69b7ceb3c
km4.kim <kyoomok@naver.com>
2024-06-13 14:52:04 +0900

    [refactoring] 66

--- game.py
+++ game.py
@@ -7,17 +7,27 @@ class Game:
 
     def guess(self, guess_num) -> GameResult:
         self.assert_illegal_value(guess_num)
-        if self.question == guess_num:
-            return GameResult(True, 3, 0)
+        if self.is_solved(guess_num):
+            return self.get_success_game_result()
         else:
-            strikes = 0
-            balls = 0
-            for i in range(len(self.question)):
-                if self.question.find(guess_num[i]) == i:
-                    strikes += 1
-                elif self.question.find(guess_num[i]) != -1:
-                    balls += 1
-            return GameResult(False, strikes, balls)
+            return self.get_unsolved_game_result(guess_num)
+
+    def get_unsolved_game_result(self, guess_num):
+        strikes = 0
+        balls = 0
+        for i in range(len(self.question)):
+            if self.question.find(guess_num[i]) == i:
+                strikes += 1
+            elif self.question.find(guess_num[i]) != -1:
+                balls += 1
+        return GameResult(False, strikes, balls)
+
+    def get_success_game_result(self):
+        return GameResult(True, 3, 0)
+
+    def is_solved(self, guess_num):
+        return self.question == guess_num
+
     def assert_illegal_value(self, guess_num):
         if guess_num is None:
             raise TypeError()
